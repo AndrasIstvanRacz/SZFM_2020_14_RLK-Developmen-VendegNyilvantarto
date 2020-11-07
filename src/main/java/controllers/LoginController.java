98b5dc4e852b08a0bba8e4ca8ea1706f99f0dfa2
AndrasIstvanRacz <43450139+AndrasIstvanRacz@users.noreply.github.com>
2020-11-07 13:59:24 +0100

    authentication metódus létrehozása

--- src/main/java/controllers/LoginController.java
+++ src/main/java/controllers/LoginController.java
@@ -1,10 +1,13 @@
 package controllers;
 
+
 import java.io.IOException;
 
 import app.App;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 
@@ -12,15 +15,27 @@ import javafx.scene.input.KeyEvent;
 public class LoginController {
 
     @FXML
+    private TextField tfUsername;
+
+    @FXML
+    private PasswordField pfPassword;
+
+    @FXML
     void loginWithEnter(KeyEvent event) {
         if(event.getCode().equals(KeyCode.ENTER)){
-            System.out.println("OK");
+            authentication();
         }
     }
 
     @FXML
     void loginWithLoginButton(ActionEvent event) {
-        System.out.println("OK");
+        authentication();
     }
 
+    void authentication(){
+        if(tfUsername.getText().equals("admin")
+            && pfPassword.getText().equals("admin")){
+            System.out.println("OK");
+        }
+    }
 }
