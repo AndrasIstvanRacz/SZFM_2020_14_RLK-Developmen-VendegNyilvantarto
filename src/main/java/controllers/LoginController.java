081c3953cc952209aaaf9bf8474c4c5f9bfb1ad5
AndrasIstvanRacz <43450139+AndrasIstvanRacz@users.noreply.github.com>
2020-11-07 15:37:29 +0100

    LoginController launchApplication metódusának megírása.

--- src/main/java/controllers/LoginController.java
+++ src/main/java/controllers/LoginController.java
@@ -1,19 +1,28 @@
 package controllers;
 
-
-import java.io.IOException;
-
-import app.App;
+import app.Main;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+import javassist.bytecode.stackmap.BasicBlock;
+import org.tinylog.Logger;
+
+import java.io.IOException;
 
 
 public class LoginController {
 
+    @FXML
+    private Pane login;
     @FXML
     private TextField tfUsername;
 
@@ -35,7 +44,25 @@ public class LoginController {
     void authentication(){
         if(tfUsername.getText().equals("admin")
             && pfPassword.getText().equals("admin")){
-            System.out.println("OK");
+            Stage stage  = (Stage) login.getScene().getWindow();
+            stage.close();
+            launchApplication("AdminWindow");
         }
+    }
+
+    void launchApplication (String appInterfaceType) {
+        try {
+            BorderPane root = FXMLLoader.load(Main.class.getResource(appInterfaceType +".fxml"));
+            Scene scene = new Scene(root);
+            Stage stage = new Stage();
+            stage.setScene(scene);
+            stage.getIcons().add(new Image("Images/Icon.png"));
+            stage.setTitle("Vendég nyílvántartó");
+            stage.show();
+        } catch (IOException ex ) {
+            Logger.error("FXML loading failed.");
+        }
+
+
     }
 }
