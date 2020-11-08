b5cd543cd03deb7596dc8e842790ff68bd1e330c
asder <lbalazs96@citromail.hu>
2020-11-08 12:02:33 +0100

    Az adatbázis változóinak a létrehozása a Permissions osztályban.

--- src/main/java/model/Permissions.java
+++ src/main/java/model/Permissions.java
@@ -1,12 +1,32 @@
 package model;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+import javax.persistence.criteria.CriteriaBuilder;
+import java.util.Date;
+
 @Data
+@Entity
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Table( name = "permissions")
 public class Permissions {
 
+    @Id
+    @Column(name = "username")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String username;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "permission")
+    private Integer permission;
 }
+
+
