6499db45de44c843ce6997fc58677b2bc3eebccd
AndrasIstvanRacz <43450139+AndrasIstvanRacz@users.noreply.github.com>
2020-11-24 14:44:45 +0100

    EmployeeWindowController handelAdd és handelEdit és a hozzá szükséges openAddOrEdit metodus megírása.

--- src/main/java/controllers/EmployeeWindowController.java
+++ src/main/java/controllers/EmployeeWindowController.java
@@ -1,6 +1,56 @@
 package controllers;
 
+import app.Main;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import org.tinylog.Logger;
+
+import java.io.IOException;
 
 public class EmployeeWindowController {
+
+    public static String AddOrEdit;
+
+    @FXML
+    private BorderPane employee;
+
+    @FXML
+    void handleAdd() {
+        openAddOrEdit("Add");
+    }
+
+    @FXML
+    void handleEdit() {
+        openAddOrEdit("Edit");
+    }
+
+    void openAddOrEdit (String type) {
+        AddOrEdit = type;
+        try {
+            Stage parentStage  = (Stage) employee.getScene().getWindow();
+            AnchorPane root = FXMLLoader.load(Main.class.getResource("EmployeeAddAndEditWindow.fxml"));
+            Scene scene = new Scene(root);
+            Stage stage = new Stage();
+            stage.initModality(Modality.WINDOW_MODAL);
+            stage.initOwner(parentStage);
+            stage.setScene(scene);
+            stage.setResizable(false);
+            stage.getIcons().add(new Image("images/Icon.png"));
+            stage.setTitle(type + " Employee");
+            stage.show();
+            Logger.info("Launch application success");
+        } catch (IOException ex ) {
+            Logger.error("Launch application failed");
+        }
+
+
+    }
 }
