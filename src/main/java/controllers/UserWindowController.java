617c8a4f24780e1a9f8dbdadd00f43128e522e33
AndrasIstvanRacz <43450139+AndrasIstvanRacz@users.noreply.github.com>
2020-11-24 14:47:44 +0100

    UserWindowController handelAdd és handelEdit és a hozzá szükséges openAddOrEdit metodus megírása.

--- src/main/java/controllers/UserWindowController.java
+++ src/main/java/controllers/UserWindowController.java
@@ -1,14 +1,23 @@
 package controllers;
 
+import app.Main;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
+import org.tinylog.Logger;
 
+import java.io.IOException;
 
 
 public class UserWindowController {
+
+    public static String AddOrEdit;
 
     @FXML
     private BorderPane user;
@@ -23,6 +32,38 @@ public class UserWindowController {
         } else {
             btnEdit.setDisable(true);
         }
+
+    }
+
+    @FXML
+    void handleAdd() {
+        openAddOrEdit("Add");
+    }
+
+    @FXML
+    void handleEdit() {
+        openAddOrEdit("Edit");
+    }
+
+    void openAddOrEdit (String type) {
+        AddOrEdit = type;
+        try {
+            Stage parentStage  = (Stage) user.getScene().getWindow();
+            AnchorPane root = FXMLLoader.load(Main.class.getResource("UserAddAndEditWindow.fxml"));
+            Scene scene = new Scene(root);
+            Stage stage = new Stage();
+            stage.initModality(Modality.WINDOW_MODAL);
+            stage.initOwner(parentStage);
+            stage.setScene(scene);
+            stage.setResizable(false);
+            stage.getIcons().add(new Image("images/Icon.png"));
+            stage.setTitle(type + " Employee");
+            stage.show();
+            Logger.info("Launch application success");
+        } catch (IOException ex ) {
+            Logger.error("Launch application failed");
+        }
+
 
     }
 }
