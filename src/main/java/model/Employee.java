f6e7f5756e94744658b40dec7a6c4d184ec3bf0f
asder <lbalazs96@citromail.hu>
2020-11-27 14:02:54 +0100

    A változókhoz létrehoztam a szükséges gettereket és settereket az Employee osztályban.

--- src/main/java/model/Employee.java
+++ src/main/java/model/Employee.java
@@ -42,6 +42,67 @@ public class Employee {
     @Column(name = "email")
     private String email;
 
+    public String getUsername() {
+        return username;
+    }
 
+    public void setUsername(String username) {
+        this.username = username;
+    }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getZip_code() {
+        return zip_code;
+    }
+
+    public void setZip_code(Integer zip_code) {
+        this.zip_code = zip_code;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getHouse_number() {
+        return house_number;
+    }
+
+    public void setHouse_number(String house_number) {
+        this.house_number = house_number;
+    }
+
+    public Integer getPhone_number() {
+        return phone_number;
+    }
+
+    public void setPhone_number(Integer phone_number) {
+        this.phone_number = phone_number;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
