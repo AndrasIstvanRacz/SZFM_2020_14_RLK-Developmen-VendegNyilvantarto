bd3fe00688b33400ee3975cc3d7851d6616f3796
asder <lbalazs96@citromail.hu>
2020-11-27 16:33:41 +0100

    A findByColumn metódus elkészítése.

--- src/main/java/database/EmployeeRepository.java
+++ src/main/java/database/EmployeeRepository.java
@@ -1,4 +1,45 @@
 package database;
 
+import model.Employee;
+import org.tinylog.Logger;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+
 public class EmployeeRepository {
+
+    public List<Employee> findByColumn(String selectedColumn, String entity) {
+        EntityManager em = EmfGetter.getEntityManager();
+        CriteriaBuilder cb = em.getCriteriaBuilder();
+
+        CriteriaQuery<Employee> cq = cb.createQuery(Employee.class);
+        Root<Employee> from = cq.from(Employee.class);
+        if(entity.isEmpty()){
+            cq.select(from);
+        } else {
+            if (selectedColumn.equals("zip_code") || selectedColumn.equals("phone_number"))  {
+                Integer number = Integer.parseInt(entity);
+                cq.select(from).where(cb.equal(from.get(selectedColumn), number));
+            } else {
+                cq.select(from).where(cb.like(from.get(selectedColumn), "%" + entity + "%"));
+            }
+        }
+        try {
+            Query q = em.createQuery(cq);
+            Logger.info("Select completed successful");
+            return q.getResultList();
+        } catch (Exception e) {
+            Logger.error("Select failed");
+        } finally {
+            em.close();
+        }
+        return new ArrayList<>();
+    }
 }
