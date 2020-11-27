31caa668022d0a73c490813c1c09da0b3c12b0a4
asder <lbalazs96@citromail.hu>
2020-11-27 14:01:21 +0100

    Az adatbázis változóinak a létrehozása az Employee osztályban.

--- src/main/java/model/Employee.java
+++ src/main/java/model/Employee.java
@@ -1,11 +1,47 @@
 package model;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+import javax.persistence.criteria.CriteriaBuilder;
+import java.util.Date;
+
 @Data
+@Entity
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Table( name = "employee")
 public class Employee {
+    @Id
+    @Column(name = "username")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String username;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "zip_code")
+    private Integer zip_code;
+
+    @Column(name = "city")
+    private String city;
+
+    @Column(name = "street")
+    private String street;
+
+    @Column(name = "house_number")
+    private String house_number;
+
+    @Column(name = "phone_number")
+    private Integer phone_number;
+
+    @Column(name = "email")
+    private String email;
+
+
+
 }
