fb262d88a413fa2557966bfe0b8bb4c64d42a26c
asder <lbalazs96@citromail.hu>
2020-11-27 17:28:38 +0100

    FindByUsername metódus megírása a PermissionsRepository.java állományban.

--- src/main/java/database/PermissionsRepository.java
+++ src/main/java/database/PermissionsRepository.java
@@ -1,6 +1,34 @@
 package database;
 
 import model.Permissions;
+import org.tinylog.Logger;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 
 public class PermissionsRepository extends GenericDb<Permissions> {
+
+    public Permissions findByUsername(String name){
+        EntityManager em = EmfGetter.getEntityManager();
+        CriteriaBuilder cb = em.getCriteriaBuilder();
+
+        CriteriaQuery<Permissions> cq = cb.createQuery(Permissions.class);
+
+        Root<Permissions> from = cq.from(Permissions.class);
+
+        cq.select(from).where(cb.like(from.get("username"),name));
+
+        try{
+            Query q = em.createQuery(cq);
+            return (Permissions) q.getResultList().get(0);
+        }catch (Exception e){
+            Logger.error("Hiba a modell megtalálása közben.");
+        }finally {
+            em.close();
+        }
+        return null;
+    }
 }
