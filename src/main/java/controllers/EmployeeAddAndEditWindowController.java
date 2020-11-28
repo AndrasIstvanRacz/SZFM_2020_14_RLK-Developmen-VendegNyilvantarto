488cd8d3544cd8d9c13f8227f900daebb42da48f
kzoltan99 <kzotya99@gmail.com>
2020-11-28 21:10:07 +0100

    handleClickSave fejlesztése

--- src/main/java/controllers/EmployeeAddAndEditWindowController.java
+++ src/main/java/controllers/EmployeeAddAndEditWindowController.java
@@ -3,6 +3,7 @@ package controllers;
 import database.EmfGetter;
 import database.EmployeeRepository;
 import database.GenericDb;
+import database.PermissionsRepository;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
@@ -13,6 +14,8 @@ import model.Permissions;
 import org.tinylog.Logger;
 
 import javax.persistence.EntityManager;
+
+import java.io.IOException;
 
 import static java.lang.Integer.parseInt;
 
@@ -43,9 +46,14 @@ public class EmployeeAddAndEditWindowController {
     private TextField tfHouseNumber;
 
     @FXML
+    private TextField tfPermission;
+
+    @FXML
     private Button btnDelete;
 
     private EmployeeRepository employeeRepository = new EmployeeRepository();
+
+    private PermissionsRepository permissionsRepository = new PermissionsRepository();
 
     @FXML
     protected void initialize(){
@@ -68,9 +76,7 @@ public class EmployeeAddAndEditWindowController {
 
         try {
             Employee newEmployee = new Employee();
-            Permissions newPermission = new Permissions();
 
-            EntityManager em = EmfGetter.getEntityManager();
             newEmployee.setEmployeeUsername(tfUsername.getText().trim());
             newEmployee.setName(tfName.getText().trim());
             newEmployee.setPhone_number(parseInt(tfPhoneNumber.getText().trim()));
@@ -79,6 +85,22 @@ public class EmployeeAddAndEditWindowController {
             newEmployee.setCity(tfCity.getText().trim());
             newEmployee.setStreet(tfStreetName.getText().trim());
             newEmployee.setHouse_number(tfHouseNumber.getText().trim());
+
+            //newPermission.setEmployeeUsername(tfUsername.getText().trim());
+
+            employeeRepository.createNew(newEmployee);
+            //newEmployee = employeeRepository.findByColumn("employeeUsername", tfUsername.getText().trim()).get(0);
+            Permissions permissions = new Permissions();
+            permissions.setEmployeeUsername(newEmployee.getEmployeeUsername());
+            permissions.setPermissionUser(newEmployee);
+            permissions.setPermission(Integer.parseInt(tfPermission.getText().trim()));
+            newEmployee.setPermissions(permissions);
+            //employeeRepository.commitChange(newEmployee);
+            permissionsRepository.createNew(permissions);
+
+
+            //employeeRepository.commitChange(newEmployee);
+
 
             tfUsername.clear();
             tfName.clear();
@@ -89,13 +111,12 @@ public class EmployeeAddAndEditWindowController {
             tfStreetName.clear();
             tfHouseNumber.clear();
 
-            employeeRepository.createNew(newEmployee);
         } catch (Exception e){
             Logger.error("Inserting invalid type");
             Alert alert = new Alert(Alert.AlertType.ERROR);
-            alert.setTitle("Hiba üzenet");
+            alert.setTitle("Error message");
             alert.setHeaderText(null);
-            alert.setContentText("Érvénytelen típus adat vagy hibás adatbázis kapcsolat.");
+            alert.setContentText("Invalid data type or bad database connection.");
             alert.showAndWait();
         }
 
