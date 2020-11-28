17436e91d7da24af1287419198d886f8ffb5bb1c
kzoltan99 <kzotya99@gmail.com>
2020-11-28 21:11:59 +0100

    Employee.java fejlesztése

--- src/main/java/model/Employee.java
+++ src/main/java/model/Employee.java
@@ -4,49 +4,38 @@ import lombok.*;
 
 import javax.persistence.*;
 import javax.persistence.criteria.CriteriaBuilder;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 @Data
 @Entity
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-@Table( name = "employee")
+@Table( name = "EMPLOYEE")
 public class Employee {
 
     @Id
-    @Column(name = "username")
     private String employeeUsername;
 
-    @OneToOne(mappedBy = "employee")
+    @OneToOne(mappedBy = "permissionUser")
     private Permissions permissions;
 
-    @Column(name = "name")
     private String name;
 
-    @Column(name = "zip_code")
     private Integer zip_code;
 
-    @Column(name = "city")
     private String city;
 
-    @Column(name = "street")
     private String street;
 
-    @Column(name = "house_number")
     private String house_number;
 
-    @Column(name = "phone_number")
     private Integer phone_number;
 
-    @Column(name = "email")
     private String email;
 
-    @OneToMany(mappedBy = "employee")
-    Set<Guest> guests = new HashSet<>();
+    @OneToMany(mappedBy = "receptionist")
+    List<Guest> guests = new ArrayList<>();
 
     @Override
     public String toString() {
