31eb8a0c7649dd41e1a3ccc5a742ae8bdf6df3fa
kzoltan99 <kzotya99@gmail.com>
2020-11-29 15:51:51 +0100

    Employee.java getterek, setterek hozzáadása

--- src/main/java/model/Employee.java
+++ src/main/java/model/Employee.java
@@ -6,7 +6,7 @@ import javax.persistence.*;
 import javax.persistence.criteria.CriteriaBuilder;
 import java.util.*;
 
-@Data
+//@Data
 @Entity
 @Builder
 @NoArgsConstructor
@@ -51,7 +51,75 @@ public class Employee {
                 '}';
     }
 
+    public String getEmployeeUsername() {
+        return employeeUsername;
+    }
 
+    public void setEmployeeUsername(String employeeUsername) {
+        this.employeeUsername = employeeUsername;
+    }
 
+    public Permissions getPermissions() {
+        return permissions;
+    }
 
+    public void setPermissions(Permissions permissions) {
+        this.permissions = permissions;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getZip_code() {
+        return zip_code;
+    }
+
+    public void setZip_code(Integer zip_code) {
+        this.zip_code = zip_code;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getHouse_number() {
+        return house_number;
+    }
+
+    public void setHouse_number(String house_number) {
+        this.house_number = house_number;
+    }
+
+    public Integer getPhone_number() {
+        return phone_number;
+    }
+
+    public void setPhone_number(Integer phone_number) {
+        this.phone_number = phone_number;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
