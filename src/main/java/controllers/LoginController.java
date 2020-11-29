9b3ec87d6f51a8640c1501f84a59a8ab2bbae86a
kzoltan99 <kzotya99@gmail.com>
2020-11-29 17:14:33 +0100

    LoginController.java authentication metódus fejlesztése

--- src/main/java/controllers/LoginController.java
+++ src/main/java/controllers/LoginController.java
@@ -2,10 +2,13 @@ package controllers;
 
 import app.Main;
 import database.EmfGetter;
+import database.PermissionsRepository;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
@@ -14,6 +17,7 @@ import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+import model.Permissions;
 import org.tinylog.Logger;
 
 import java.io.IOException;
@@ -30,11 +34,16 @@ public class LoginController {
     private TextField tfUsername;
 
     @FXML
+    private Label labelWarning;
+
+    @FXML
     private PasswordField pfPassword;
+
+    private PermissionsRepository permissionsRepository = new PermissionsRepository();
 
     @FXML
     void initialize(){
-        EmfGetter.asd();
+        EmfGetter.openEmf();
     }
     @FXML
     void loginWithEnter(KeyEvent event) {
@@ -49,17 +58,45 @@ public class LoginController {
     }
 
     void authentication(){
+
         if(tfUsername.getText().equals("admin")
                 && pfPassword.getText().equals("admin")){
             Stage stage  = (Stage) login.getScene().getWindow();
             stage.close();
             launchApplication("AdminWindow", "(Admin)");
         }
-        else if (tfUsername.getText().equals("Rabsz Olga")
-                && pfPassword.getText().equals("rOlga")){
-            Stage stage  = (Stage) login.getScene().getWindow();
-            stage.close();
-            launchApplication("UserWindow", "(User)");
+
+        Permissions userPermission;
+        userPermission = permissionsRepository.findByUsername(tfUsername.getText());
+
+        if (userPermission != null && userPermission.getPassword() == null) {
+            userPermission.setPassword(pfPassword.getText());
+            permissionsRepository.commitChange(userPermission);
+
+        }
+        else {
+            try{
+                userPermission = permissionsRepository.findByUsername(tfUsername.getText());
+                if (userPermission.getEmployeeUsername().equals(tfUsername.getText())
+                        && userPermission.getPassword().equals(pfPassword.getText())) {
+                    if (userPermission.getPermission() == 1) {
+                        Stage stage = (Stage) login.getScene().getWindow();
+                        stage.close();
+                        launchApplication("AdminWindow", "(Admin)");
+                    } else if (userPermission.getPermission() == 0) {
+                        Stage stage = (Stage) login.getScene().getWindow();
+                        stage.close();
+                        launchApplication("UserWindow", "(User)");
+                    }
+                }
+                else{
+                    labelWarning.setVisible(true);
+                }
+            }catch (NullPointerException ex){
+                labelWarning.setVisible(true);
+            }
+
+
         }
     }
 
