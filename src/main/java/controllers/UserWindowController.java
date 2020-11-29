51d432ccaf4cfa58ad77b6b6f5342b4c1b574443
kzoltan99 <kzotya99@gmail.com>
2020-11-29 19:50:26 +0100

    UserWindowController.java handleClickSearch metódus megírása

--- src/main/java/controllers/UserWindowController.java
+++ src/main/java/controllers/UserWindowController.java
@@ -1,18 +1,27 @@
 package controllers;
 
 import app.Main;
+import database.EmployeeRepository;
+import database.GuestRepository;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.image.Image;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
+import model.Employee;
+import model.Guest;
 import org.tinylog.Logger;
 
 import java.io.IOException;
+import java.util.Date;
 
 
 public class UserWindowController {
@@ -26,11 +35,78 @@ public class UserWindowController {
     private Button btnEdit;
 
     @FXML
+    private TableView<Guest> guestTable;
+
+    @FXML
+    private TableColumn<Guest, Integer> columnId;
+
+    @FXML
+    private TableColumn<Guest, String> columnName;
+
+    @FXML
+    private TableColumn<Guest, Integer> columnPhoneNumber;
+
+    @FXML
+    private TableColumn<Guest, String> columnEmail;
+
+    @FXML
+    private TableColumn<Guest, Date> columnStartDate;
+
+    @FXML
+    private TableColumn<Guest, Date> columnEndDate;
+
+    @FXML
+    private TableColumn<Guest, String> columnRoomType;
+
+    @FXML
+    private TableColumn<Guest, Integer> columnPayment;
+
+    @FXML
+    private ComboBox<String> cbBox;
+
+    @FXML
+    private TextField tfSearch;
+
+    private GuestRepository guestRepository = new GuestRepository();
+
+    @FXML
     protected void initialize(){
         if(LoginController.userType.equals("(Admin)")){
             btnEdit.setDisable(false);
         } else {
             btnEdit.setDisable(true);
+        }
+
+    }
+
+    private void initColumn() {
+        columnId.setCellValueFactory(new PropertyValueFactory<>("id"));
+        columnName.setCellValueFactory(new PropertyValueFactory<>("name"));
+        columnPhoneNumber.setCellValueFactory(new PropertyValueFactory<>("phone_number"));
+        columnEmail.setCellValueFactory(new PropertyValueFactory<>("email"));
+        columnStartDate.setCellValueFactory(new PropertyValueFactory<>("occupying_the_room"));
+        columnEndDate.setCellValueFactory(new PropertyValueFactory<>("leaving_the_room"));
+        columnRoomType.setCellValueFactory(new PropertyValueFactory<>("room_type"));
+        columnPayment.setCellValueFactory(new PropertyValueFactory<>("payment"));
+    }
+
+    @FXML
+    void handleClickSearch() {
+        try {
+            ObservableList<Guest> data = FXCollections.observableArrayList(
+                    guestRepository.findByColumn(getColumnName(cbBox.getValue().trim()),
+                            tfSearch.getText().trim()));
+            tfSearch.clear();
+            guestTable.setItems(data);
+            initColumn();
+
+        }catch (Exception e){
+            Logger.error("Search by invalid type");
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Error message");
+            alert.setHeaderText(null);
+            alert.setContentText("Invalid data type or bad database connection.");
+            alert.showAndWait();
         }
 
     }
@@ -66,4 +142,34 @@ public class UserWindowController {
 
 
     }
+
+    private String getColumnName(String name) {
+        String columnName;
+        if (name.equals("Id")) {
+            columnName = "id";
+            return columnName;
+        } else if (name.equals("Name")) {
+            columnName = "name";
+            return columnName;
+        } else if (name.equals("Phone number")) {
+            columnName = "phone_number";
+            return columnName;
+        } else if (name.equals("E-mail")) {
+            columnName = "email";
+            return columnName;
+        } else if (name.equals("Start date")) {
+            columnName = "occupying_the_room";
+            return columnName;
+        } else if (name.equals("End date")) {
+            columnName = "leaving_the_room";
+            return columnName;
+        } else if (name.equals("Room type")) {
+            columnName = "room_type";
+            return columnName;
+        } else{
+            columnName = "payment";
+            return columnName;
+        }
+    }
+
 }
